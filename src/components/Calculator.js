a80d37b7f85a2cc54778d5eae7e9d61df062dbdc
tvililax <thomasf.dacosta@gmail.com>
2019-11-22 13:45:09 +0100

    Adding Plus Operator.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,6 +7,8 @@ class Calculator extends Component {
         super(props);
         this.state = {
             numberDisplay : '',
+            piles: [],
+            result: 0,
         }
     }
 
@@ -23,29 +25,78 @@ class Calculator extends Component {
                 <button onClick={() => this.displaySelection(7)}> 7 </button> 
                 <button onClick={() => this.displaySelection(8)}> 8 </button> 
                 <button onClick={() => this.displaySelection(9)}> 9 </button> <br/>
-                <button > + </button> 
-                <button > - </button> 
-                <button > * </button> <br/>
+                <button onClick={() => this.plusOperator(this.state.numberDisplay)}> + </button> 
+                <button onClick={() => this.minusOperator()}> - </button> 
+                <button onClick={() => this.multipleOperator()}> * </button> <br/>
 
                 <button onClick={() => this.pushList(this.state.numberDisplay)}>Entrer</button>
 
-                <p> {this.state.numberDisplay}</p>
+                <p> {this.state.numberDisplay} </p>
+                <p> {this.state.result} </p>
 
             </div>
         )
     }
 
+    // fonction qui permet l'affichage en direct à chaque numéro cliqué
     displaySelection = (num) => {
+
         this.setState((prevState) => ({
             numberDisplay: prevState.numberDisplay+num
         }))
+
     }
 
+    // fonction qui permet de pousser un bouton cliqué dans une pile
     pushList = (pile) => {
+
         console.log(pile);
-        this.setState((prevState) => ({
+
+        this.state.piles.push(pile);
+        this.setState(() => ({
+            numberDisplay: ''
+        }));
+
+        console.log('tableau piles');
+        console.log(this.state.piles);
+    }
+
+    // fonction qui permet d'additionner le nombre entré, avec celui/ceux déjà dans la pile
+    plusOperator = (num) => {
+
+        console.log("on va additionner "+this.state.piles+" avec "+num);
+
+        for (let i=0; i<this.state.piles.length;i++){
+            this.state.result += parseInt(this.state.piles[i])
+        }
+
+        this.state.result += parseInt(num);
+        console.log("resultat : "+this.state.result)
+
+        this.setState(() => ({
+            result: this.state.result,
             numberDisplay: ''
         }))
+
+    }
+
+    // fonction qui permet de soustraire le nombre déjà dans la pile par celui entré
+    plusOperator = (num) => {
+
+        console.log("on va additionner "+this.state.piles+" avec "+num);
+
+        for (let i=0; i<this.state.piles.length;i++){
+            this.state.result += parseInt(this.state.piles[i])
+        }
+
+        this.state.result += parseInt(num);
+        console.log("resultat : "+this.state.result)
+
+        this.setState(() => ({
+            result: this.state.result,
+            numberDisplay: ''
+        }))
+
     }
 
 }
